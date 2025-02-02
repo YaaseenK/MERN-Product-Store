65c5dc811a7339519f5f43ff77bc5a5de3cc44af
yaaseenk <yaaseenkhan02@gmail.com>
2025-02-02 04:13:50 -0500

    had to install npm install -g win-node-env cos it wasnt reconizing NODE_ENV

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,12 @@ app.use(express.json()); // Parse JSON bodies
 
 app.use("/api/products", productRoutes);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-} 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     connectDB()
