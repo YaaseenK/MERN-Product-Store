1a22eff1d0a93085a016f725dac2b85e56ae8c0e
yaaseenk <yaaseenkhan02@gmail.com>
2025-02-01 20:41:53 -0500

    connected to the db and created a product model. also created a functin to post products to the db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from "./config/db.js"
+import Product  from "./models/product.model.js"
+
+dotenv.config();
 
 const app = express();
-
 const PORT = 3000;
 
+app.post('/products' , (req, res) => {
+    try {
+        const createProduct = async (req, res) =>{
+            const product = await Product.create(req.body)
+            res.status(200).json(product);
+        }
+    } catch (err) {
+        return res.status(400).json({ message: `Error:  ${err.message}` });
+    }
+});
+
+
 app.listen(PORT, () => {
+    connectDB()
     console.log("Server started at http://localhost:3000")
 })
